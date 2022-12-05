ff39fa017186b2d7b3329b54d64bba9381487e96
Dmitry Shuranov <dvshur@gmail.com>
2022-12-05 06:43:36 +0300

    sender support for new db

--- src/bin/sender.rs
+++ src/bin/sender.rs
@@ -1,11 +1,7 @@
 use chrono::{DateTime, Utc};
 use diesel::{prelude::*, Connection, PgConnection};
-use lib::{
-    config::Config,
-    schema::{devices, messages},
-    Error,
-};
-use std::{collections::HashMap, time::Duration};
+use lib::{config::Config, Error};
+use std::time::Duration;
 
 // todo proper logging
 
@@ -17,27 +13,40 @@ async fn main() -> Result<(), Error> {
     let mut conn = PgConnection::establish(&config.postgres.database_url())?;
 
     // get message from queue
-    let message_to_send = dequeue(&mut conn)?;
+    let message_to_send = postgres::dequeue(&mut conn)?;
 
     match message_to_send {
         None => tokio::time::sleep(Duration::from_secs(EMPTY_QUEUE_POLL_PERIOD_SECS)).await,
-        Some(msg) => {
-            // todo ttl
-            let message = fcm_message(&config.fcm_api_key, &msg);
+        Some(message) => {
+            // todo check eligibility to send using exponential backoff
+            // for now, no retries. 1 attempt per message
+            if message.send_attempts_count > 0 {
+                postgres::skip(&mut conn, message.uid, Utc::now())?;
+            } else {
+                // message is eligible for sending
+                let fcm_msg = formatter::message(&config.fcm_api_key, &message);
 
-            // todo post-send actions
-            // match fcm::Client::new().send(message).await.map(|_| ()) {
-            match Ok::<_, fcm::FcmError>(message).map(|_| ()) {
-                Ok(()) => {
-                    println!("Send succesful message UID {}", msg.uid);
-
-                    diesel::delete(messages::table.filter(messages::uid.eq(msg.uid)))
-                        .execute(&mut conn)?;
-
-                    println!("Message {} deleted from DB", msg.uid);
-                }
-                Err(err) => eprintln!("Send error: {:?}. TODO set updated_at, sending_error", err),
-            };
+                // todo ttl
+                // match fcm::Client::new().send(fcm_msg).await.map(|_| ()) {
+                match Ok::<fcm::Message, fcm::FcmError>(fcm_msg).map(|_| ()) {
+                    Ok(()) => {
+                        println!("SUCCESS | Message {}", message.uid);
+                        postgres::ack(&mut conn, message.uid)?;
+                        println!("Message {} deleted from DB", message.uid);
+                    }
+                    Err(err) => {
+                        eprintln!("ERROR | Message {} | {:?}", message.uid, err);
+                        postgres::nack(
+                            &mut conn,
+                            message.uid,
+                            message.send_attempts_count + 1,
+                            format!("{:?}", err),
+                            Utc::now(),
+                        )?;
+                        println!("Message {} nack", message.uid);
+                    }
+                };
+            }
         }
     }
 
@@ -45,65 +54,121 @@ async fn main() -> Result<(), Error> {
 }
 
 #[derive(Debug, Clone, Queryable)]
-struct MessageToSend {
+pub struct MessageToSend {
     pub uid: i32,
-    pub _created_at: DateTime<Utc>,
-    pub _updated_at: DateTime<Utc>,
-    pub _send_error: Option<String>,
-    pub _send_attempts_count: i32,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+    pub send_error: Option<String>,
+    pub send_attempts_count: i32,
     pub notification_title: String,
     pub notification_body: String,
-    pub _collapse_key: Option<String>,
+    pub collapse_key: Option<String>,
     pub fcm_uid: String,
 }
 
-fn dequeue(conn: &mut PgConnection) -> Result<Option<MessageToSend>, Error> {
-    Ok(messages::table
-        .inner_join(devices::table.on(messages::device_uid.eq(devices::uid)))
-        .select((
-            messages::uid,
-            messages::created_at,
-            messages::updated_at,
-            messages::send_error,
-            messages::send_attempts_count,
-            messages::notification_title,
-            messages::notification_body,
-            messages::collapse_key,
-            devices::fcm_uid,
-        ))
-        .order(messages::updated_at)
-        .first(conn)
-        .optional()?)
-}
-
-fn fcm_message<'a>(fcm_api_key: &'a str, message_to_send: &'a MessageToSend) -> fcm::Message<'a> {
-    let notification = {
-        let mut builder = fcm::NotificationBuilder::new();
-        builder.title(&message_to_send.notification_title);
-        builder.body(&message_to_send.notification_body);
-        builder.finalize()
+mod postgres {
+    use crate::MessageToSend;
+    use chrono::{DateTime, Utc};
+    use diesel::{prelude::*, PgConnection};
+    use lib::{
+        schema::{devices, messages},
+        Error,
     };
 
-    let mut builder = fcm::MessageBuilder::new(fcm_api_key.as_ref(), &message_to_send.fcm_uid);
-    builder.notification(notification);
-    builder.data(&HashMap::<String, String>::new()).unwrap(); // message must have `data` field, or empty object as a minimum
+    /// Sets message updated_at.
+    /// Use to send message to the end of the queue with no action
+    pub fn skip(
+        conn: &mut PgConnection,
+        message_uid: i32,
+        new_updated_at: DateTime<Utc>,
+    ) -> Result<(), Error> {
+        diesel::update(messages::table)
+            .filter(messages::uid.eq(message_uid))
+            .set(messages::updated_at.eq(new_updated_at))
+            .execute(conn)?;
+        Ok(())
+    }
 
-    // todo collapse key
-    // if let Some(k) = collapse_key {
-    // builder.collapse_key(&k);
-    // }
+    pub fn nack(
+        conn: &mut PgConnection,
+        message_uid: i32,
+        new_send_attempts_count: i32,
+        new_send_error: String,
+        new_updated_at: DateTime<Utc>,
+    ) -> Result<(), Error> {
+        diesel::update(messages::table)
+            .filter(messages::uid.eq(message_uid))
+            .set((
+                messages::updated_at.eq(new_updated_at),
+                messages::send_attempts_count.eq(new_send_attempts_count),
+                messages::send_error.eq(new_send_error),
+            ))
+            .execute(conn)?;
+        Ok(())
+    }
 
-    // todo ttl
-    // todo priority
+    pub fn ack(conn: &mut PgConnection, message_uid: i32) -> Result<(), Error> {
+        diesel::delete(messages::table.filter(messages::uid.eq(message_uid))).execute(conn)?;
+        Ok(())
+    }
 
-    builder.finalize()
+    pub fn dequeue(conn: &mut PgConnection) -> Result<Option<MessageToSend>, Error> {
+        Ok(messages::table
+            .inner_join(devices::table.on(messages::device_uid.eq(devices::uid)))
+            .select((
+                messages::uid,
+                messages::created_at,
+                messages::updated_at,
+                messages::send_error,
+                messages::send_attempts_count,
+                messages::notification_title,
+                messages::notification_body,
+                messages::collapse_key,
+                devices::fcm_uid,
+            ))
+            .filter(messages::send_attempts_count.lt(1)) // todo retries
+            .order(messages::updated_at)
+            .first(conn)
+            .optional()?)
+    }
+}
+
+mod formatter {
+    use crate::MessageToSend;
+    use std::collections::HashMap;
+
+    pub fn message<'a>(
+        fcm_api_key: &'a str,
+        message_to_send: &'a MessageToSend,
+    ) -> fcm::Message<'a> {
+        let notification = {
+            let mut builder = fcm::NotificationBuilder::new();
+            builder.title(&message_to_send.notification_title);
+            builder.body(&message_to_send.notification_body);
+            builder.finalize()
+        };
+
+        let mut builder = fcm::MessageBuilder::new(fcm_api_key.as_ref(), &message_to_send.fcm_uid);
+        builder.notification(notification);
+        builder.data(&HashMap::<String, String>::new()).unwrap(); // message must have `data` field, or empty object as a minimum
+
+        // todo collapse key
+        // if let Some(k) = collapse_key {
+        // builder.collapse_key(&k);
+        // }
+
+        // todo ttl
+        // todo priority
+
+        builder.finalize()
+    }
 }
 
 // #[tokio::test]
 // async fn get_msg() {
 //     let config = Config::load().unwrap();
 //     let mut conn = PgConnection::establish(&config.postgres.database_url()).unwrap();
-//     let msg = dequeue(&mut conn).unwrap().unwrap();
+//     let msg = postgres::dequeue(&mut conn).unwrap().unwrap();
 //     assert_eq!(msg.uid, 1);
 //     assert_eq!(msg.fcm_uid, "uid_0");
 // }
