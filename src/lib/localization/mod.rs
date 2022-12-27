bdd346650819e63373f8e1f2b1130d9446c14706
Alex Kordys <akordys@web3tech.ru>
2022-12-27 15:49:08 +0300

    Localization: refactoring - extracted gateway to a separate file

--- src/lib/localization/mod.rs
+++ src/lib/localization/mod.rs
@@ -10,103 +10,14 @@ use self::localise_gateway::{dto::KeysResponse, RemoteGateway, LOCALISE_API_URL}
 
 pub use self::localise_gateway::LokaliseConfig;
 
+mod localise_gateway;
+
 mod lokalise_keys {
     pub const ORDER_FILLED_TITLE: &str = "orderFilledTitle";
     pub const ORDER_FILLED_MSG: &str = "orderFilledMessage";
     pub const ORDER_PART_FILLED_MSG: &str = "orderPartFilledMessage";
     pub const PRICE_ALERT_TITLE: &str = "priceAlertTitle";
     pub const PRICE_ALERT_MSG: &str = "priceAlertMessage";
-}
-
-mod localise_gateway {
-    use crate::error::Error;
-    use wavesexchange_apis::HttpClient;
-
-    pub(super) const LOCALISE_API_URL: &str = "https://api.lokalise.co/api2";
-
-    pub struct LokaliseConfig {
-        pub token: String,
-        pub project_id: String,
-    }
-
-    pub(super) struct RemoteGateway {
-        lokalise_client: HttpClient<()>,
-    }
-
-    impl RemoteGateway {
-        pub fn new(api_url: &str, api_token: &str) -> Self {
-            let lokalise_client = HttpClient::<()>::builder()
-                .with_base_url(api_url)
-                .with_reqwest_builder(|req| {
-                    use reqwest::header::{HeaderMap, HeaderValue};
-                    let mut headers = HeaderMap::new();
-                    let value = |s| HeaderValue::from_str(s).expect("api token");
-                    headers.insert("X-Api-Token", value(api_token));
-                    req.default_headers(headers)
-                })
-                .build();
-            RemoteGateway { lokalise_client }
-        }
-
-        pub async fn keys_for_project(&self, project_id: &str) -> Result<dto::KeysResponse, Error> {
-            self.lokalise_client
-                .create_req_handler::<dto::KeysResponse>(
-                    self.lokalise_client
-                        .http_get(format!("projects/{project_id}/keys?include_translations=1",)),
-                    "lokalise::get",
-                )
-                .execute()
-                .await
-                .map_err(Error::from)
-        }
-    }
-
-    pub(super) mod dto {
-        use serde::Deserialize;
-
-        #[derive(Debug, Clone, Deserialize)]
-        pub struct KeysResponse {
-            pub project_id: String,
-            pub keys: Vec<Key>,
-        }
-
-        #[derive(Debug, Clone, Deserialize)]
-        pub struct Key {
-            pub key_id: i64,
-            pub created_at: String,
-            pub created_at_timestamp: i64,
-            pub key_name: PlatformStrings,
-            pub filenames: PlatformStrings,
-            pub description: String,
-            pub platforms: Vec<String>,
-            pub tags: Vec<String>,
-            pub translations: Option<Vec<Translation>>,
-        }
-
-        #[derive(Debug, Clone, Deserialize)]
-        pub struct PlatformStrings {
-            pub ios: String,
-            pub android: String,
-            pub web: String,
-            pub other: String,
-        }
-
-        #[derive(Debug, Clone, Deserialize)]
-        pub struct Translation {
-            pub translation_id: i64,
-            pub key_id: i64,
-            pub language_iso: String,
-            pub translation: String,
-            pub modified_by: i64,
-            pub modified_by_email: String,
-            pub modified_at: String,
-            pub modified_at_timestamp: i64,
-            pub is_reviewed: bool,
-            pub is_unverified: bool,
-            pub reviewed_by: i64,
-            pub task_id: Option<i64>,
-        }
-    }
 }
 
 type Key = String;
